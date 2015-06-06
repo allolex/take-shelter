1c2c8136f4a9dae4ea65dbd2d6f3cc8ad8a72fbe
Bryce Kerley <bkerley@brycekerley.net>
2015-06-06 16:04:56 -0400

    find hurricane shelters by zipcode

--- app/models/hurricaneshelter.rb
+++ app/models/hurricaneshelter.rb
@@ -2,4 +2,14 @@ class Hurricaneshelter < ActiveRecord::Base
   def self.distance_order(lat_lon)
     order("ST_Distance(ST_SetSRID(#{lat_lon.as_postgis}, 900914), wkb_geometry) DESC")
   end
+
+  def self.by_zipcode(zip)
+    unless zip.is_a? Zipcode
+      # will puke if the zip is neither numeric or string, i'm okay with that
+      zip.gsub!(/\-.+/,'') unless zip.is_a? Numeric
+      zip = Zipcode.find_by zipcode: zip
+    end
+
+    distance_order zip.as_latlon
+  end
 end
