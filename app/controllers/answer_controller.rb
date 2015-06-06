397c3f8b4302dbaee85c77cceeb133115f6cacd4
Damon Davison <damon@curatur.com>
2015-06-06 15:22:31 -0400

    Fix Twilio header

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,6 +1,8 @@
 class AnswerController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  after_filter :set_header
+
   def query
     render plain: "OK"
   end
@@ -10,5 +12,11 @@ class AnswerController < ApplicationController
       r.Message I18n.t(:stay_with_friends, scope: :shelter)
     end
     render plain: twiml.text
+  end
+
+  private
+
+  def set_header
+    response.headers["Content-Type"] = "text/xml"
   end
 end
