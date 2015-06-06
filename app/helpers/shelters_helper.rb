06a0a424e9a6665d89cb82947129854875d6ffad
Bryce Kerley <bkerley@brycekerley.net>
2015-06-06 16:41:27 -0400

    show coords and a map

--- app/helpers/shelters_helper.rb
+++ app/helpers/shelters_helper.rb
@@ -1,2 +1,12 @@
 module SheltersHelper
+  def static_map(shelters)
+    latlons = shelters.map(&:as_latlon)
+    centroid = LatLon.centroid latlons
+    markers = latlons.map(&:to_marker)
+    params = {markers: markers,
+              center: centroid.to_marker,
+              zoom: 2}.to_query
+    uri = "http://staticmap.openstreetmap.de/staticmap.php?#{params}"
+    image_tag uri
+  end
 end
