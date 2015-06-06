39ac0bbcf0720f895448f4ddfb729aa6977b5bc6
Damon Davison <damon@curatur.com>
2015-06-06 14:58:55 -0400

    Configure controller and route for Twilio

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,4 +1,6 @@
 class AnswerController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def query
     render plain: "OK"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'answer/query' => "answer#query"
 
-  get 'answer/message' => "answer#message"
+  post 'answer/message' => "answer#message"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
