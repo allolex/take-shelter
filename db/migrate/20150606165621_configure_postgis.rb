4853241679be47cc1dc570a3a03f3da604fcfc56
Bryce Kerley <bkerley@brycekerley.net>
2015-06-06 15:07:23 -0400

    postgis configured correctly

--- db/migrate/20150606165621_configure_postgis.rb
+++ db/migrate/20150606165621_configure_postgis.rb
@@ -4,6 +4,9 @@ class ConfigurePostgis < ActiveRecord::Migration
   end
 
   def down
+    ActiveRecord::Base.connection.execute <<-SQL
+      DELETE FROM spatial_ref_sys WHERE srid=102658;
+    SQL
     disable_extension 'postgis'
   end
 end
