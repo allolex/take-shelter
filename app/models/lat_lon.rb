ecec776240082147b5aeb0777538818d9a20369c
Bryce Kerley <bkerley@brycekerley.net>
2015-06-06 15:32:24 -0400

    document lat_lon class

--- app/models/lat_lon.rb
+++ app/models/lat_lon.rb
@@ -1,3 +1,5 @@
+# A class to abstract a latitude and longitude, to prevent swapping the two
+# fields.
 class LatLon < Struct.new(:lat, :lon)
   def as_postgis
     "ST_MakePoint(#{lat.to_f}, #{lon.to_f})"
