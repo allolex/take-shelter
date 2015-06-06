05bdbdca352bcf2d80f0c55fc6e1ada3efe5764e
Bryce Kerley <bkerley@brycekerley.net>
2015-06-06 16:41:13 -0400

    centroid and formatting for latlons

--- app/models/lat_lon.rb
+++ app/models/lat_lon.rb
@@ -1,7 +1,39 @@
 # A class to abstract a latitude and longitude, to prevent swapping the two
 # fields.
 class LatLon < Struct.new(:lat, :lon)
+  def self.centroid(collection)
+    lat_centroid = collection.map(&:lat).sum / collection.length
+    lon_centroid = collection.map(&:lon).sum / collection.length
+
+    new.tap do |ll|
+      ll.lat = lat_centroid
+      ll.lon = lon_centroid
+    end
+  end
+
   def as_postgis
     "ST_MakePoint(#{lat.to_f}, #{lon.to_f})"
+  end
+
+  def to_s
+    [lat_s, lon_s].join ', '
+  end
+
+  def to_marker
+    [lat, lon].join ','
+  end
+
+  def lat_s
+    [
+      lat.abs.round(5),
+      (lat > 0 ? 'N' : 'S')
+    ].join ' '
+  end
+
+  def lon_s
+    [
+      lon.abs.round(5),
+      (lon > 0 ? 'E' : 'W')
+    ].join ' '
   end
 end
